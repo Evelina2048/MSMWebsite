2fb97b3850cd1f9a7cfad5ccae541c7af87b7e6a
Evelina2048 <evelinaguenther@gmail.com>
2024-12-26 16:21:46 -0500

    "test" button submits what it is supposed to

--- MSMWebsiteReact/src/MultiSelectDropdown.jsx
+++ MSMWebsiteReact/src/MultiSelectDropdown.jsx
@@ -10,45 +10,29 @@ const options = [
 
 function MultiSelectDropdown() {
 
-    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [selectedOptions, setSelectedOptions] = useState([]); //setSelectedOptions defined here
     const handleChange = (selectedOption) => {
         setSelectedOptions(selectedOption);
     };
+    const submit = () => {
+        //console.log(selectedOptions);
+        const selectedLabels = selectedOptions.map(option => option.label);
+        console.log(selectedLabels);
+    }
 
     return (
         <div className="multiSelectDropdown">
-        <h5>Multi select dropdown</h5>
+        {/* <h5>Multi select dropdown</h5> */}
         {/* Dropdown */}
         <Select
                 options= {options} //{/* options to be in dropdown */}
-                value= {selectedOptions} //{/* selected options in dropdown is vlaue of selectedOptions */}
+                value= {selectedOptions} //{/* selected options in dropdown is value of selectedOptions */}
                 onChange= {handleChange}
                 isMulti = {true} //{/*to determine if multiselect*/}
         />
-        {/* <button type="button">Test</button> */}
+        <button type="button" onClick={submit}>Test</button>
         </div>
     );
-
-    // const handleClick = () => {
-    //     //gather data from "sort by dropdown"
-    // }
-
-    // return(
-    //     <div className ="submit">
-    //     <button typ="button" onClick={handleClick}>Test</button>
-    //     </div>
-    // );
-
-
-        //     <Select
-        //         options= {options} //{/* options to be in dropdown */}
-        //         value= {selectedOptions} //{/* selected options in dropdown is vlaue of selectedOptions */}
-        //         onChange= {handleChange}
-        //         //isMulti = {true} //{/*to determine if multiselect*/}
-        //     />
-        //     <button>Submitty</button>
-        //     </div>
-        //<div className ="submit">
 }
 
 export default MultiSelectDropdown
