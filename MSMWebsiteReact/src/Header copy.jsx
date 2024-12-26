58d15994afd063e72a4da6c1238c39784deaddd6
Evelina2048 <evelinaguenther@gmail.com>
2024-12-25 19:19:08 -0500

    got all

---
+++ MSMWebsiteReact/src/Header copy.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+
+    return(
+        <header className ="header"//style={{
+        //     display: 'flex',
+        //     alignItems: 'center',
+        //     justifyContent: 'center',
+        // }}
+        >
+            <h2>
+                <u>MSM Breeding Combonations2</u>
+            </h2>
+        </header>
+
+    );
+}
+
+export default Header
--- MSMWebsiteReact/src/MultiSelectDropdown.jsx
+++ MSMWebsiteReact/src/MultiSelectDropdown.jsx
@@ -1,28 +1,54 @@
+//import React from 'react'
+import React, { useState } from "react";
 import Select from "react-select";
 
+const options = [
+    { value: "plant", label: "Plant" },
+    { value: "cold", label: "Cold" },
+    { value: "air", label: "Air" },
+];
 
 function MultiSelectDropdown() {
 
-    const selectOptions = [
-        { value: "plant", label: "Plant"},
-        { value: "cold", label: "Cold"},
-        { value: "air", label: "Air"}
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const handleChange = (selectedOption) => {
+        setSelectedOptions(selectedOption);
+    };
 
-]
-    const [selectedOptions, setSelectedOptions] = useState([]) //use an array
-    const handleChange = (selectOption) => {
-        setSelectedOptions(selectOption);
-    }
-    return(
-        <div className ="multiSelectDropdown">
+    return (
+        <div className="multiSelectDropdown">
         <h5>Multi select dropdown</h5>
-        {/*Dropdown*/}
-        options={options} /* options to be in dropdown */
-        value={selectedOptions} /* selected options in dropdown is vlaue of selectedOptions */
-        onChange={handleChange}
-        isMulti{true} /*to determine if multiselect*/
+        {/* Dropdown */}
+        <Select
+                options= {options} //{/* options to be in dropdown */}
+                value= {selectedOptions} //{/* selected options in dropdown is vlaue of selectedOptions */}
+                onChange= {handleChange}
+                isMulti = {true} //{/*to determine if multiselect*/}
+        />
+        {/* <button type="button">Test</button> */}
         </div>
     );
+
+    // const handleClick = () => {
+    //     //gather data from "sort by dropdown"
+    // }
+
+    // return(
+    //     <div className ="submit">
+    //     <button typ="button" onClick={handleClick}>Test</button>
+    //     </div>
+    // );
+
+
+        //     <Select
+        //         options= {options} //{/* options to be in dropdown */}
+        //         value= {selectedOptions} //{/* selected options in dropdown is vlaue of selectedOptions */}
+        //         onChange= {handleChange}
+        //         //isMulti = {true} //{/*to determine if multiselect*/}
+        //     />
+        //     <button>Submitty</button>
+        //     </div>
+        //<div className ="submit">
 }
 
 export default MultiSelectDropdown
