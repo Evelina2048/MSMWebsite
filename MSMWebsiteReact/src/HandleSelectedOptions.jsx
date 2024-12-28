58847a4233cbe94733d7529d91040c27b948c0c2
Evelina2048 <evelinaguenther@gmail.com>
2024-12-28 15:40:17 -0500

    got selectedopts printed in handleselopts.jsx

--- MSMWebsiteReact/src/HandleSelectedOptions.jsx
+++ MSMWebsiteReact/src/HandleSelectedOptions.jsx
@@ -2,8 +2,9 @@
 
 function HandleSelectedOptions({ selectedOptions }) {
     console.log("hi before return")
+    console.log("hi, these are your selected labels", selectedOptions.map(option => option.label)) //.map creates a new array by applying a function to each element in the original array.
     return(
-        console.log("hi from handleselectedopts")//(getSelectedLabels)
+        console.log("hi from handleselectedopts")
     );
 }
 export default HandleSelectedOptions
