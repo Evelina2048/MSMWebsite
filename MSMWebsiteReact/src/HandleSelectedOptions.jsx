a2cee6ed7f8550b3675cf4d2ed68f6a3c39b0e81
Evelina2048 <evelinaguenther@gmail.com>
2025-01-04 18:35:14 -0500

    Entbrat shows on website

--- MSMWebsiteReact/src/HandleSelectedOptions.jsx
+++ MSMWebsiteReact/src/HandleSelectedOptions.jsx
@@ -1,10 +1,19 @@
 //import { getSelectedLabels } from "./MultiSelectDropdown";
+import normEntbrat from './assets/NormEntbrat.webp';
+
 
 function HandleSelectedOptions({ selectedOptions }) {
     console.log("hi before return")
     console.log("hi, these are your selected labels", selectedOptions.map(option => option.label)) //.map creates a new array by applying a function to each element in the original array.
     return(
-        console.log("hi from handleselectedopts")
+        // console.log("hi from handleselectedopts")
+        <img src = {normEntbrat} alt="Normal Entbrat" />
+        // for (opts in selectedOptions) {
+        //     console.log(opts)
+        //     if (opts.equals("Plant")) {
+        //         System.out.println("im a plant")
+        //     }
+        // }
     );
 }
 export default HandleSelectedOptions
