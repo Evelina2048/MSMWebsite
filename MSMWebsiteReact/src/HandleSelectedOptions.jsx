22fe78d9ca601c89ecb02e20ed647a481c803bf8
Evelina2048 <evelinaguenther@gmail.com>
2025-01-08 19:12:26 -0500

    push images if its attributes are in options label

--- MSMWebsiteReact/src/HandleSelectedOptions.jsx
+++ MSMWebsiteReact/src/HandleSelectedOptions.jsx
@@ -10,7 +10,7 @@ function HandleSelectedOptions({ selectedOptions }) {
         normEntbrat: { //object
             src: normEntbrat,
             alt: "Norm Entbrat",
-            attributes: ["plant island","snow","rock"]
+            attributes: ["Plant Island","snow","rock"]
         },
         // {
         //     src: "normEntbrat",
@@ -33,13 +33,31 @@ function HandleSelectedOptions({ selectedOptions }) {
     //         console.log("something else ", option.label);
     //     }
     // });
-
+    const imagesToDisplay = []
     return(
         selectedOptions.map(option => { //use map to loop over selectedOptions. For each option, checks if the label is "Plant Island".
+            console.log("before, image length ",imagesToDisplay.length, " images length ", images.length)
+            //
+            Object.values(images).forEach(image => {
+                if (image.attributes.includes(option.label)) {
+                    imagesToDisplay.push(image)
+                    console.log("yayyyy")
+                }
+            })
+            //
             if (option.label === "Plant Island") {
+                //then do another loop through image attributes to check for anything that contains "plant island" as an attribute
+                // for (let imageIndex = 0; imageIndex < Object.keys(images).length; imageIndex++) {
+                //     for(let attributeIndex = 0; attributeIndex< Object.keys(im).attributes.length; attributeIndex++) {
+                //         console.log("attribute: ", images[imageIndex].attributes[attributeIndex]);
+                //         imagesToDisplay.push(images[imageIndex])
+                //     }
+                // }
+                console.log("after,image length ",imagesToDisplay.length)
+                //if yes, add to list
                 return(
                     <div className="normEntbrat">
-                        <img src = {normEntbrat} alt="Normal Entbrat" />
+                        <img src = {images.normEntbrat.src} alt={images.normEntbrat.alt} />
                         <p>+</p>
                         <p>____________</p>
                     </div>
