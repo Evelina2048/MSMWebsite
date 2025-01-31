02f6113e8f7cd17b2121de1063a0ec16ddc5d364
Evelina2048 <evelinaguenther@gmail.com>
2025-01-31 18:07:22 -0500

    Moved images over to JSON

--- MSMWebsiteReact/src/HandleSelectedOptions.jsx
+++ MSMWebsiteReact/src/HandleSelectedOptions.jsx
@@ -1,142 +1,65 @@
-//import { getSelectedLabels } from "./MultiSelectDropdown";
-import normDrumpler from './assets/NormDrumpler.webp';
-import normEntbrat from './assets/NormEntbrat.webp';
-import normFwog from './assets/NormFwog.webp';
-import normFurcorn from './assets/NormFurcorn.webp';
-import normMaw from './assets/NormMaw.webp';
-import normOaktopus from './assets/NormOaktopus.webp';
-import normShrubb from './assets/NormShrubb.webp';
-import normPotbelly from './assets/NormPotbelly.webp'
-import normNoggin from './assets/NormNoggin.webp'
-
+import monsterImages from './monsterImages.json';
 
 const listOfFilteredIslands = []
 
 function HandleSelectedOptions({ selectedOptions }) {
     console.log("hi, these are your selected labels", selectedOptions.map(option => option.label)) //.map creates a new array by applying a function to each element in the original array.
+    console.log("that ")
+    let imagesToDisplay = PopulateImagesToDisplay(selectedOptions, monsterImages);
 
-    const images = {
-        normEntbrat: { //object
-            src: normEntbrat,
-            alt: "Normal Entbrat",
-            attributes: ["Plant Island", "Snow (Element)","Earth (Element)","Water (Element)","Plant (Element)"],
-            PlantIsland: [[normShrubb,normMaw], [normOaktopus,normDrumpler], [normFurcorn,normFwog]] //all monster combonations that can make an entbrat on plant island
-            // "normClamble normToeJammer"], ["normPummel normMammott"]]
-        },
+    console.log("filtered islands "+listOfFilteredIslands);
+    if (listOfFilteredIslands.length>0) {
+        let islandArray = imagesToDisplay[0].Islands[listOfFilteredIslands[0].replace(/\s+/g, "")][0]; //gives first combonation for entbrat (first in images to display)
+        // console.log("**** "+imagesToDisplay[0].Islands.PlantIsland);
+    }
 
-        normShrubb: {
-            src: normShrubb,
-            alt: "Normal Shrubb",
-            attributes: ["Plant Island", "Plant (Element)","rock"],
-            PlantIsland: [[normPotbelly,normNoggin]]
-        },
-
-        normMaw: {
-            src: normMaw,
-            alt: "Normal Maw",
-            attributes: ["Snow (element)"],
-            PlantIsland: ["idk"]
-        },
-
-        normOaktopus: {
-            src: normOaktopus,
-            alt: "Normal Maw",
-            attributes: ["idk"],
-            PlantIsland: ["idk"]
-        },
-
-        normNoggin: {
-            src: normNoggin,
-            alt: "",
-            attributes: [""],
-            PlantIsland: [""],
-        },
-
-        normPotbelly: {
-            src: normPotbelly,
-            alt: "",
-            attributes: [""],
-            PlantIsland: [""],
-        },
-        // {
-        //     src: "normEntbrat",
-        //     alt: "Normal Entbrat",
-        //     attributes: ["plant island","snow","rock"],
-        // },
-    };
-    const imagesToDisplay = PopulateImagesToDisplay(selectedOptions, images);
-    console.log("that ",imagesToDisplay[0][listOfFilteredIslands[0].replace(/\s+/g, "")]);
-
-    let islandArray = imagesToDisplay[0][listOfFilteredIslands[0].replace(/\s+/g, "")][0]; //gives first combonation for entbrat (first in images to display)
-    console.log("island 3333 "+islandArray);
-    //
-
+    console.log("2222 imagesToDisplay",imagesToDisplay);
     return(
-        //
+
         imagesToDisplay.map((image) => {
-            return image.PlantIsland.map((combonation) => {
-            //System.out.println("hi")
-            // console.log("combonation "+combonation);
-            return iterateThroughImages(combonation, image); //combo image
-            
-            
+            console.log("3333 ");
+            if (listOfFilteredIslands.length > 0) {
+                return image.Islands[listOfFilteredIslands[0].replace(/\s+/g, "")].map((combonation) => {
+                    return iterateThroughImages(combonation, image); //combo image
+                })
+            }
+
+            else {
+                //listOfAllIslands or iterate through parts of src? Or from whatever index onward
+                return iterateThroughIslandCombinations(image);//iterateThroughImages(imagesToDisplay, image);
+            }
         })
-    })
     )
-
-    // return (
-    //     imagesToDisplay.map((image, index) => ( //imagesToDisplay.map((image, index) => {
-    //         //image.PlantIsland.map((combonation) => {
-    //         console.log("island 2222 "+combonation+ Array.isArray(combonation));
-            
-    //         // return iterateThroughImages(combonation, image);
-
-    //         <div className="allImageEquation"> 
-    //             <div className="normEntbrat">
-    //                 <img src={islandArray[0]} alt={"idk"} />
-    //                 <p className="plusSign">+</p>
-    //                 <img src={islandArray[1]} alt={"idk"} />
-    //                 <div className="equalSign">
-    //                     <p>=</p>
-    //                 </div>
-    //                 <img src={image.src} alt={image.alt} />
-    //             </div>
-    //         </div>
-    //     )
-    // )
-    // )
-    //
 }
     export default HandleSelectedOptions
 
     function PopulateImagesToDisplay(selectedOptions, images){
-        console.log("Attributes for normEntbrat:", images["normEntbrat"].attributes);
-        console.log("plant combs for normEntbrat:", images["normEntbrat"].PlantIsland);
-
-        const imagesToDisplay = []
-        // const listOfFilteredIslands = []
+        console.log("that 2222 "+selectedOptions.map(option => option.label))
+        let imagesToDisplay = []
         let hasHadAnImagePassOneFilter = false
         let selectedOptionsIndex = -1;
         selectedOptions.some((option,index) => { //use map to loop over selectedOptions. For each option, checks if the label is "Plant Island".
-                console.log("before, image length ",imagesToDisplay.length, " images length ", images.length, " images to display size ", imagesToDisplay.length)
-                Object.values(images).forEach(image => {
-                    if (image.attributes.includes(option.label)) { //if exploring possibilities with first option
+            Object.values(images).forEach(image => {
+                    if (image.attributes.includes(option.label) && (!imagesToDisplay.includes(image))) { //if (image.attributes.includes(option.label)) //if exploring possibilities with first option
                         imagesToDisplay.push(image)
-                        console.log("yayyyy ",Object.values(imagesToDisplay).length,"options index ", index)
                         hasHadAnImagePassOneFilter=true
                     }
                     if(option.label.includes("Island")) {
-                        console.log("includesisland")
+                        console.log("includesisland "+option.label)
                         listOfFilteredIslands.push(option.label)
+                        console.log("testlength "+listOfFilteredIslands.length)
                     }
                     if(hasHadAnImagePassOneFilter == true) {
                         selectedOptionsIndex = index;
+                        console.log("selected options 6666 "+listOfFilteredIslands.length+" "+imagesToDisplay.length);
                         return;
                     }
+
                 },
                 )
             })
         if(selectedOptionsIndex != -1) {
+            console.log("selected options 1111 "+selectedOptions)
             for(let i = selectedOptionsIndex; i < selectedOptions.length; i++) {
                 let option = selectedOptions[i];
                 if(option.label.includes("Island")) {
@@ -146,44 +69,51 @@ function HandleSelectedOptions({ selectedOptions }) {
                     if (!image.attributes.includes(option.label)) {
                         imagesToDisplay.remove(image);
                     }
+
                 })
+
             }
         }
-        // console.log("11111 ",imagesToDisplay[0].src.[listOfFilteredIslands[0]])
-        console.log("11111 ",imagesToDisplay[0][listOfFilteredIslands[0].replace(/\s+/g, "")]) //regex to replace all spaces and whitespace with ""
 
         //go through imagesToDisplay, get their combonations
         for (let i = 0; i < imagesToDisplay.length; i++) {
-            imagesToDisplay[i][listOfFilteredIslands[0].replace(/\s+/g, "")]
-            //let ooh = imagesToDisplay[0]
-            //console.log("on ", ooh[listOfFilteredIslands[0].replace(/\s+/g, "")][0])
-            console.log("back ",imagesToDisplay[0][listOfFilteredIslands[0].replace(/\s+/g, "")][0][0])
-            // <img src={normMaw} alt={"idk"} />
+            if (listOfFilteredIslands.length > 0) {
+                imagesToDisplay[i][listOfFilteredIslands[0].replace(/\s+/g, "")]
+            }
         }
-        //go through the combonations seperating at spaces. This is the sources, get images pulled up with proper formatting
 
-
-        // images[imagesToDisplay[0].src].listOfFilteredIslands[0]
-        //
-        //use index outside of loop to pick up from there, and refine through the rest of the options
-        //
         return imagesToDisplay;
         }
 
-    function iterateThroughImages(islandArray, image){//, index) {
-        console.log("fjovrjor "+islandArray+"     "+islandArray[0]+"      "+Array.isArray(islandArray)); //island array is undefined
+    function iterateThroughImages(combonation, image){//, index) {
+        // console.log("fjovrjor "+monsterImages["PlantIsland"].src); //island array is undefined
         return (
+        <div className = "everything">
             <div className="allImageEquation">
+                <div className = "island">
+                    <img src={monsterImages[listOfFilteredIslands[0].replace(/\s+/g, "")].src} alt={listOfFilteredIslands[0].replace(/\s+/g, "")} />
+                </div>
                 <div className="normEntbrat">
-                    <img src={islandArray[0]} alt={"idk"} />
+                    <img src={combonation[0]} alt={"idk"} />
                     <p className="plusSign">+</p>
-                    <img src={islandArray[1]} alt={"idk"} />
+                    <img src={combonation[1]} alt={"idk"} />
                     <div className="equalSign">
                         <p>=</p>
                     </div>
                     <img src={image.src} alt={image.alt} />
-                    {/* fix above line. Cant use image.src has  to be something else. Maybe another loop :( */}
                 </div>
             </div>
+        </div>
         );
+    }
+
+    function iterateThroughIslandCombinations(imageKey) {
+        console.log("test1 "+ imageKey.alt);
+        Object.entries(imageKey).map(([key,value]) => {
+            console.log("test "+ key+" value "+value);
+            if (key.includes("Island")) {
+                console.log("contains isl");
+            }
+        })
+        //go through entries
     }
--- MSMWebsiteReact/src/MultiSelectDropdown.jsx
+++ MSMWebsiteReact/src/MultiSelectDropdown.jsx
@@ -5,10 +5,16 @@ import Select from "react-select";
 import HandleSelectedOptions from './HandleSelectedOptions.jsx'
 
 const options = [
-    { value: "plant", label: "Plant Island" },
-    { value: "cold", label: "Cold" },
+    { value: "plant island", label: "Plant Island" },
+    { value: "cold island", label: "Cold Island" },
     { value: "air", label: "Air" },
-    { value: "plant(Element)", label: "Plant (Element)" }
+    { value: "plant(Element)", label: "Plant (Element)" },
+    { value: "rare", label: "Rare" },
+    { value: "rareOaktopus", label: "Rare Oaktopus" },
+    { value: "rareFwog", label: "Rare Fwog" },
+    { value: "rareMaw", label: "Rare Maw" },
+    { value: "rareToeJammer", label: "Rare Toe Jammer" },
+    //{value: "", label: ""},
 ];
 
 const getSelectedLabels = (selectedOptions) => {
@@ -48,3 +54,93 @@ function MultiSelectDropdown() {
 }
 
 export default MultiSelectDropdown
+
+
+
+  // {
+  //   "normBowgart": {
+  //     "src": "images/NormBowgart.webp",
+  //     "alt": "Normal Bowgart",
+  //     "attributes": [],
+  //     "PlantIsland": [],
+  //     "ColdIsland": []
+  //   },
+  //   "normCongle": {
+  //     "src": "images/NormCongle.webp",
+  //     "alt": "Normal Congle",
+  //     "attributes": [],
+  //     "AirIsland": []
+  //   },
+  //   "normEntbrat": {
+  //     "src": "images/NormEntbrat.webp",
+  //     "alt": "Normal Entbrat",
+  //     "attributes": ["Plant Island", "Snow (Element)", "Earth (Element)", "Water (Element)", "Plant (Element)"],
+  //     "PlantIsland": [["images/NormShrubb.webp", "images/NormMaw.webp"], ["images/NormOaktopus.webp", "images/NormDrumpler.webp"], ["images/NormFurcorn.webp", "images/NormFwog.webp"]]
+  //   },
+  //   "normShrubb": {
+  //     "src": "images/NormShrubb.webp",
+  //     "alt": "Normal Shrubb",
+  //     "attributes": ["Plant Island", "Plant (Element)", "Rock"],
+  //     "PlantIsland": [["images/NormPotbelly.webp", "images/NormNoggin.webp"]]
+  //   },
+  //   "normMammott": {
+  //     "src": "images/NormMammott.webp",
+  //     "alt": "Normal Mammott",
+  //     "attributes": [],
+  //     "PlantIsland": [],
+  //     "ColdIsland": []
+  //   },
+  //   "normMaw": {
+  //     "src": "images/NormMaw.webp",
+  //     "alt": "Normal Maw",
+  //     "attributes": ["Snow (Element)"],
+  //     "PlantIsland": ["idk"]
+  //   },
+  //   "normOaktopus": {
+  //     "src": "images/NormOaktopus.webp",
+  //     "alt": "Normal Oaktopus",
+  //     "attributes": ["idk"],
+  //     "PlantIsland": ["idk"]
+  //   },
+  //   "rareFwog": {
+  //     "src": "images/RareFwog.webp",
+  //     "alt": "Rare Fwog",
+  //     "attributes": ["Plant Island", "Earth (Element)", "Water (Element)", "Rare Fwog", "Rare"],
+  //     "PlantIsland": [["images/NormNoggin.webp", "images/NormToeJammer.webp"]],
+  //     "AirIsland": [["images/NormTRox.webp", "images/NormCongle.webp"]]
+  //   },
+  //   "rareOaktopus": {
+  //       "src": "images/RareOaktopus.webp",
+  //       "alt": "Rare Oaktopus",
+  //       "attributes": ["Plant Island", "Plant (Element)", "Water (Element)", "Rare Oaktopus", "Rare"],
+  //       "PlantIsland": [["images/NormPotbelly.webp", "images/NormToeJammer.webp"]],
+  //       "ColdIsland": [["images/NormPotbelly.webp", "images/NormToeJammer.webp"]]
+  //     },
+  //     "rareMaw": {
+  //       "src": "images/RareMaw.webp",
+  //       "alt": "Rare Maw",
+  //       "attributes": ["Plant Island", "Cold Island", "Rare", "Water (Element)", "Snow (Element)"],
+  //       "PlantIsland": [["images/NormToeJammer.webp", "images/NormMammott.webp"]],
+  //       "ColdIsland": [["images/NormToeJammer.webp", "images/NormMammott.webp"]]
+  //     },
+  //     "rareToeJammer": {
+  //       "src": "images/RareToeJammer.webp",
+  //       "alt": "Rare Toe Jammer",
+  //       "attributes": ["Plant Island", "Cold Island", "Rare", "Water (Element)"],
+  //       "Islands": {
+  //       "PlantIsland": [["images/NormBowgart.webp", "images/NormTRox.webp"]], 
+  //       "ColdIsland": []
+  //       }
+  //     },
+  //     "PlantIsland": {
+  //       "src": "images/PlantIsland.webp",
+  //       "alt": "Plant Island Wordmark",
+  //       "attributes": ["PlantIsland"]
+  //     },
+
+  //     "ColdIsland": {
+  //       "src": "images/ColdIsland.webp",
+  //       "alt": "Cold Island Wordmark",
+  //       "attributes": ["ColdIsland"]
+  //     }
+  // }
