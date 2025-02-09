f9e18639c3435c4bb0a3d2b410c0afa6e301dce8
Evelina2048 <evelinaguenther@gmail.com>
2025-02-09 14:26:34 -0500

    sorting for only combinations for rare monsters

--- MSMWebsiteReact/src/HandleSelectedOptions.jsx
+++ MSMWebsiteReact/src/HandleSelectedOptions.jsx
@@ -15,17 +15,36 @@ function HandleSelectedOptions({ selectedOptions }) {
 
     console.log("2222 imagesToDisplay",imagesToDisplay);
     return(
-
         imagesToDisplay.map((image) => {
             console.log("3333 ");
             if (listOfFilteredIslands.length > 0) {
                 return image.Islands[listOfFilteredIslands[0].replace(/\s+/g, "")].map((combonation) => {
+                    console.log("no "+combonation);
                     return iterateThroughImages(combonation, image); //combo image
                 })
             }
 
             else {
                 //listOfAllIslands or iterate through parts of src? Or from whatever index onward
+                console.log("moment "+image.Islands.PlantIsland);
+
+
+                console.log("enough "+Object.keys(image.Islands));//.map((combination) => combination[0]));
+                return Object.keys(image.Islands).map((island) => {
+                    console.log("brothers "+island);
+                    console.log("thing "+island);
+                    return image.Islands[island].map((combination) => {
+                        // console.log("combination: ", combination)
+                        return iterateThroughIslandCombinations(combination, image, island);//iterateThroughImages(combination, image);
+                    }
+                    );
+
+                    //console.log("hthtth "+);
+                    // Object.entries(combonation).map(([key,value]) => {
+                    //     console.log("testDos "+ key+" value "+value);
+                    // })
+                })
+
                 return iterateThroughIslandCombinations(image);//iterateThroughImages(imagesToDisplay, image);
             }
         })
@@ -65,14 +84,16 @@ function HandleSelectedOptions({ selectedOptions }) {
                 if(option.label.includes("Island")) {
                     listOfFilteredIslands.push(option.label)
                 }
-                imagesToDisplay.forEach((image) => { //for each image in imagesToDisplay, remove if it doesn't match option
-                    if (!image.attributes.includes(option.label)) {
-                        imagesToDisplay.remove(image);
-                    }
+                // imagesToDisplay.forEach((image) => { //for each image in imagesToDisplay, remove if it doesn't match option
+                //     if (!image.attributes.includes(option.label)) {
+                //         imagesToDisplay.remove(image);
+                //     }
 
-                })
-
-            }
+                // })
+                imagesToDisplay.filter((image) => (
+                    console.log("2/9")));
+                    //!image.attributes.includes(option.label)));
+                }
         }
 
         //go through imagesToDisplay, get their combonations
@@ -86,7 +107,7 @@ function HandleSelectedOptions({ selectedOptions }) {
         }
 
     function iterateThroughImages(combonation, image){//, index) {
-        // console.log("fjovrjor "+monsterImages["PlantIsland"].src); //island array is undefined
+        console.log("fjovrjor "+monsterImages["PlantIsland"]); //island array is undefined
         return (
         <div className = "everything">
             <div className="allImageEquation">
@@ -94,6 +115,7 @@ function HandleSelectedOptions({ selectedOptions }) {
                     <img src={monsterImages[listOfFilteredIslands[0].replace(/\s+/g, "")].src} alt={listOfFilteredIslands[0].replace(/\s+/g, "")} />
                 </div>
                 <div className="normEntbrat">
+                {/* <img src={monsterImages.PlantIsland.src} alt={"idk"} /> */}
                     <img src={combonation[0]} alt={"idk"} />
                     <p className="plusSign">+</p>
                     <img src={combonation[1]} alt={"idk"} />
@@ -107,13 +129,44 @@ function HandleSelectedOptions({ selectedOptions }) {
         );
     }
 
-    function iterateThroughIslandCombinations(imageKey) {
-        console.log("test1 "+ imageKey.alt);
-        Object.entries(imageKey).map(([key,value]) => {
-            console.log("test "+ key+" value "+value);
-            if (key.includes("Island")) {
-                console.log("contains isl");
-            }
-        })
+    function iterateThroughIslandCombinations(combination, image, island) { //imageKey) {
+        console.log("mon "+combination[0]+" island "+island);
+        //not returning properly
+        if (island != undefined) {
+            console.log("Rendering test component...");
+            // return (
+            //     <div>
+            //       <p>Test: Is this component rendering?</p>
+            //       <img src={monsterImages["PlantIsland"]?.src} alt="Testing Image" />
+            //     </div>
+            //   );
+        return (
+            <div className = "everything">
+                <div className="allImageEquation">
+                    <div className = "island">
+                        <img src={monsterImages[island].src} alt={"idk"} />
+                        {/* {monsterImages[island].src} */}
+                    </div>
+                    <div className="normEntbrat">
+                        <img src={combination[0]} alt={combination[0]} />
+                        <p className="plusSign">+</p>
+                        <img src={combination[1]} alt={"idk"} />
+                        <div className="equalSign">
+                            <p>=</p>
+                        </div>
+                        <img src={image.src} alt={"idk"} />
+                    </div>
+                </div>
+            </div>
+            );
+        }
+
+        // console.log("test1 "+ imageKey.alt);
+        // Object.entries(imageKey).map(([key,value]) => {
+        //     console.log("test "+ key+" value "+value);
+        //     if (key.includes("Island")) {
+        //         console.log("contains isl");
+        //     }
+        // })
         //go through entries
     }
